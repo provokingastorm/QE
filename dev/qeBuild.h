3f2cdaf706e297c7900dfb72408b1616496cf5f2
provokingastorm <pete.s.bennett@gmail.com>
2026-07-17 19:10:59 -0500

    Add a newer version of QE along with a few single-file test apps

---
+++ dev/qeBuild.h
@@ -0,0 +1,17 @@
+#ifndef QEBUILD_H
+#define QEBUILD_H
+
+/////////////////////////////
+// SINGLE FILE DEMO PROGRAMS
+/////////////////////////////
+
+// Add your build defines here
+
+/////////////////////////////
+// SINGLE FILE DEMO PROGRAMS
+/////////////////////////////
+#define BUILD_HELLOWORLD    1
+#define BUILD_DIAG          0
+#define BUILD_POWERBAR      0
+
+#endif // QEBUILD_H
